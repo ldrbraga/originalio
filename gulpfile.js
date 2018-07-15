274d5af31a35e5b3878edb1a36b5ba02c58e603b
ldrbraga <ldrbraga@gmail.com>
2018-07-15 00:40:27 -0300

    update build

--- gulpfile.js
+++ gulpfile.js
@@ -1,64 +1,63 @@
-var gulp = require('gulp');
+var gulp = require('gulp')
 var htmlmin = require('gulp-htmlmin')
-var sass = require('gulp-sass');
-var rename = require('gulp-rename');
-var connect = require('gulp-connect');
-var imagemin = require('gulp-imagemin');
+var sass = require('gulp-sass')
+var rename = require('gulp-rename')
+var connect = require('gulp-connect')
+var imagemin = require('gulp-imagemin')
 
-// tasks for sass
 var scssFiles = './src/scss/style.scss'; //sass src
 var cssDest = './css'; //css dest
 var cssMinDest = './dist/css' ///css.min dest
+ 
 
 var sassDevOptions = {
- outputStyle: 'expanded'
+  outputStyle: 'expanded'
 }
 var sassProdOptions = {
- outputStyle: 'compressed'
+  outputStyle: 'compressed'
 }
 
+// tasks for sass
 gulp.task('sassDev', function(){
  return gulp.src(scssFiles)
   .pipe(sass(sassDevOptions)).on('error', sass.logError)
-  .pipe(gulp.dest(cssDest));
+  .pipe(gulp.dest(cssDest))
 });
 
 gulp.task('sassProd', function(){
  return gulp.src(scssFiles)
   .pipe(sass(sassProdOptions)).on('error', sass.logError)
   .pipe(rename('style.min.css'))
-  .pipe(gulp.dest(cssMinDest));
+  .pipe(gulp.dest(cssMinDest))
 });
-
-// task watch - 'gulp watch' 
-gulp.task('watch', function(){
- gulp.watch(scssFiles, ['sassDev', 'sassProd']);
-})
 
 //task html min
-
 gulp.task('htmlmin', function(){
-	return gulp.src('*.html')
+  return gulp.src('src/*.html')
 	.pipe(htmlmin({collapseWhitespace: true}))
-	.pipe(gulp.dest('dist/'));
+  .pipe(gulp.dest('dist/'))
 });
 
-// task local server
+// task watch 
+gulp.task('watch', function(){
+ gulp.watch(scssFiles, ['sassDev', 'sassProd'])
+ gulp.watch('src/*.html', ['htmlmin'])
+})
 
+// task local server
 gulp.task('connect', function(){
- connect.server({
-  root: './',
-  livereload: true
- })
+  connect.server({
+    root: 'dist/',
+    livereload: true
+  })
 })
 
 //task image min
-
 gulp.task('imagemin', function(){
  return gulp.src('./assets/images/*')
  .pipe(imagemin())
- .pipe(gulp.dest('dist/images'));
+ .pipe(gulp.dest('dist/images'))
 })
 
 
-gulp.task('default', ['htmlmin', 'sassDev', 'sassProd', 'watch', 'imagemin', 'connect']);
+gulp.task('default', ['htmlmin', 'sassDev', 'sassProd', 'imagemin', 'connect', 'watch'])
