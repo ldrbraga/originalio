1254d4dbf8b1bd5d7248ced6633ce416c126b317
ldrbraga <ldrbraga@gmail.com>
2018-07-15 04:14:55 -0300

    build header

--- gulpfile.js
+++ gulpfile.js
@@ -5,11 +5,10 @@ var rename = require('gulp-rename')
 var connect = require('gulp-connect')
 var imagemin = require('gulp-imagemin')
 
-var scssFiles = './src/scss/style.scss'; //sass src
-var cssDest = './css'; //css dest
+var scssFiles = ['./src/scss/*.scss'] //sass src
+var cssDest = './css' //css dest
 var cssMinDest = './dist/css' ///css.min dest
  
-
 var sassDevOptions = {
   outputStyle: 'expanded'
 }
